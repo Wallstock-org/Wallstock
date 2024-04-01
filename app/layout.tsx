e2d8822cb281e3ed9100c580e6c7ea991453c836
priyanshuverma-dev <priyanshu.creator.dev@gmail.com>
2024-04-01 12:57:16 +0530

    Add new page, authentication routes, and authentication provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import AuthProvider from "@/providers/auth-provider";
+import { auth } from "@/auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,14 +15,18 @@ export const metadata: Metadata = {
     "Empower your financial journey with customizable stock market widgets and interactive charts.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthProvider session={session}>{children}</AuthProvider>
+      </body>
     </html>
   );
 }
